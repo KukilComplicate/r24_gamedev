88078840f9be72ef63a8b485d94397ecbe0991ad
Kukil Borgohain <kukil50.kb@gmail.com>
2025-08-03 13:42:35 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,15 @@
 int main()
 {
   printf("Hello!\n");
+  InitWindow(480, 270, "Hello!");
+  while(!WindowShouldClose())
+  {
+    ClearBackground(RAYWHITE);
+    BeginDrawing();
+    DrawFPS(10, 10);
+    EndDrawing();
+  }
+  CloseWindow();
   return 0;
 }
+
